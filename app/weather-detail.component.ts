86f49cca9194065ad5567a2c269e7198fdbeb2f2
Olaf Schneider <schneider.olaf.1@gmail.com>
2016-10-13 19:10:14 +0200

    Additional weather data items

--- app/weather-detail.component.ts
+++ app/weather-detail.component.ts
@@ -6,11 +6,16 @@ import {WeatherData} from './weather.data';
   template: `
     <div *ngIf="weather">
       <div><label>Temprature: </label>{{clacCelsius(weather.main.temp)}} °C</div>
+      <div><label>Temprature min: </label>{{clacCelsius(weather.main.temp_min)}} °C</div>
+      <div><label>Temprature max: </label>{{clacCelsius(weather.main.temp_max)}} °C</div>
       <div><label>Wind: </label>{{weather.wind.speed}} m/s {{weather.wind.deg}} degrees (meteorological)</div>
       <div><label>Pressure: </label>{{weather.main.pressure}} hpa</div>
       <div><label>Humidity: </label>{{weather.main.humidity}} %</div>
+      <div><label>Cloudiness: </label>{{weather.clouds.all}} %</div>
+      <div *ngIf="weather.rain"><label>Rain: </label>{{weather.rain.three_hours}} liter</div>
+      <div *ngIf="weather.snow"><label>Snow: </label>{{weather.snow.three_hours}} liter</div>
       <ul class="conditions">
-        <li *ngFor="#w of weather.weather">
+        <li *ngFor="let w of weather.weather">
             <span class="badge">{{w.description}}</span><img src="http://openweathermap.org/img/w/{{w.icon}}.png">
         </li>
       </ul>
