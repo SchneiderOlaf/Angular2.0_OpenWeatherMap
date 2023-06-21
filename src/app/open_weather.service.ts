8d38bf2cdab75c37f5e4d50b0166e4eafd1dc6d6
Olaf Schneider <schneider.olaf.1@gmail.com>
2023-06-21 21:03:57 +0200

    Updated to Angular 16.1

--- src/app/open_weather.service.ts
+++ src/app/open_weather.service.ts
@@ -1,5 +1,5 @@
 import {Injectable}     from '@angular/core';
-import {Http} from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 import {WeatherData} from './weather.data';
 import {throwError}     from 'rxjs'
 import { catchError, map } from 'rxjs/operators';
@@ -10,11 +10,11 @@ import {APP_ID} from './constants';
 @Injectable()
 export class OpenWeatherService {
     private apiId = APP_ID;
-    constructor (private http: Http) {}
+    constructor (private http: HttpClient) {}
 
     getCurrentWeather(city: string) {
         return  this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + this.apiId)
-                        .pipe(map(res => <WeatherData> res.json()))
+                        .pipe(map(res => <WeatherData> res))
                         .pipe(catchError(error => throwError( error.message || 'Server error')))
     };
 }
