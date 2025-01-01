0804a1ea9d0d227e7ef65458d47b62a1a441971b
Olaf Schneider <schneider.olaf.1@gmail.com>
2025-01-01 15:20:46 +0100

    Updated Angular CORE and CLI to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WeatherDetailComponent } from './weather-detail.component';
@@ -9,21 +9,15 @@ import { WeatherService } from './weather.service';
 import { OpenWeatherService } from './open_weather.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    WeatherDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
-  providers: [
-    WeatherService,
-    OpenWeatherService
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        WeatherDetailComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule], providers: [
+        WeatherService,
+        OpenWeatherService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
--- src/app/open_weather.service.ts
+++ src/app/open_weather.service.ts
@@ -1,5 +1,5 @@
 import {Injectable}     from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {WeatherData} from './weather.data';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators'; // Import specific operators
